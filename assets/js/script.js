85dd9ac884a4311aab93c9983cb5beac0e81f343
Pena <penawalin@gmail.com>
2024-01-15 22:11:02 +0000

    Saves Searched City to recent searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,42 @@ $(function () {
         lat: data.coord.lat,
         lon: data.coord.lon
       };
+      saveToSearchHistory(cityName);
+    });
+  }
+
+  const searchHistory = JSON.parse(localStorage.getItem('City:')) || [];
+
+  displaySearchHistory();
+
+  $('#search-form').submit(function (event) {
+    event.preventDefault();
+    
+    const cityName = $('#search-input').val().trim();
+    if (cityName !== '') {
+      buildQuery(cityName);
+    }
+  });
+
+  $('#history').on('click', '.list-group-item', function () {
+    const cityName = $(this).text();
+    buildQuery(cityName);
+  });
+
+  function saveToSearchHistory(cityName) {
+    if (!searchHistory.includes(cityName)) {
+      searchHistory.push(cityName);
+      localStorage.setItem('City:', JSON.stringify(searchHistory));
+      displaySearchHistory();
+    }
+  }
+
+  function displaySearchHistory() {
+    $('#history').empty();
+    searchHistory.forEach(function (city) {
+      const historyEl = $('<a>')
+      .addClass('list-group-item list-group-item-action').text(`${city}`);
+      $('#history').append(historyEl);
     });
   }
 });
