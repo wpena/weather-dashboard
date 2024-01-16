63a128254a58bf21ecfcbdd621b9753521c3f1c6
Pena <penawalin@gmail.com>
2024-01-16 11:49:48 +0000

    Dashboard displays the current weather forecast for searched city

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ $(function () {
         lon: data.coord.lon
       };
       saveToSearchHistory(cityName);
+      currentWeather(coordinates);
     });
   }
 
@@ -19,7 +20,7 @@ $(function () {
 
   $('#search-form').submit(function (event) {
     event.preventDefault();
-    
+
     const cityName = $('#search-input').val().trim();
     if (cityName !== '') {
       buildQuery(cityName);
@@ -43,8 +44,42 @@ $(function () {
     $('#history').empty();
     searchHistory.forEach(function (city) {
       const historyEl = $('<a>')
-      .addClass('list-group-item list-group-item-action').text(`${city}`);
+        .addClass('list-group-item list-group-item-action').text(`${city}`);
       $('#history').append(historyEl);
     });
   }
+
+  function currentWeather(coordinates) {
+    const APIKey = '25a131e364e3b7a6a6ce9928baf5b301';
+    const queryURL = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${APIKey}`;
+
+    $.get(queryURL, function (data) {
+      const weatherDiv = $('<div>');
+
+      const cityName = data.name;
+      const currentDate = dayjs().format('D/MM/YYYY');
+      const iconURL = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+      const iconAlt = data.weather[0].description;
+
+      const cityEl = $('<h2>');
+      const iconEl = $('<img>').attr('src', iconURL).attr('alt', iconAlt);
+      cityEl.append(`${cityName} (${currentDate})`, iconEl);
+      weatherDiv.append(cityEl);
+
+      const currentTemp = data.main.temp;
+      const tempEl = $('<p>').text(`Temp: ${currentTemp} °C`);
+      weatherDiv.append(tempEl);
+
+      const windspeed = data.wind.speed;
+      const windEl = $('<p>').text(`Wind: ${windspeed} KPH`);
+      weatherDiv.append(windEl);
+
+      const humidity = data.main.humidity;
+      const humidityEl = $('<p>').text(`Humidity: ${humidity}%`);
+      weatherDiv.append(humidityEl);
+
+      $('#today').html(weatherDiv);
+    });
+  }
+
 });
